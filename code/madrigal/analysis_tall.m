d5f9587e288211112890cf6ef315fd417310ab1c
Jiaen Ren <jiaenren@umich.edu>
2022-01-28 12:23:35 -0500

    adjust yscale to log

--- code/madrigal/analysis_tall.m
+++ code/madrigal/analysis_tall.m
@@ -3,10 +3,12 @@ tec_dir = '/Users/jiaenren/Dropbox (University of Michigan)/TEC_201709/Data/Madr
 year = 2005; 
 tall = tectall([tec_dir '/' num2str(year)]);
 
-%% make tec histogram
-h1 = histogram(tall.tec, 0:5:500);
-xscale('log');
-
-%% make dtec histogram
+%% make tec and dtec histogram
+ax1 = subplot(2,1,1);
+h1 = histogram(tall.tec, 0:5:600);
+ax2 = subplot(2,1,2);
 h2 = histogram(tall.dtec, 0:1:100);
-xscale('log');
+
+%% adjust scale
+ax1.YScale = 'log';
+ax2.YScale = 'log';
